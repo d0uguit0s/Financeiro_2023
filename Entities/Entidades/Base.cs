1373837c5f79a776faaf885bf2157fc52849df93
d0ugit0s <douglas.s.o676@gmail.com>
2024-07-23 21:52:02 -0300

    Criando Entidades Relacionadas

---
+++ Entities/Entidades/Base.cs
@@ -0,0 +1,12 @@
+﻿using Entities.Notificacoes;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Entidades;
+public class Base : Notifica
+{
+    [Display(Name = "Código")]
+    public Guid Id { get; set; }
+
+    [Display(Name = "Name")]
+    public string Nome { get; set; }
+}
